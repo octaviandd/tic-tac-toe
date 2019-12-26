df8d004744060b01ba1a9300ed9767b54d4d653e
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2019-12-26 12:13:37 +0000

    Added new name picking system; draw checker !

--- config.js
+++ config.js
@@ -1,148 +1,177 @@
-const positions = document.querySelectorAll('.col')
-const winningCombo = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [3,4,5],
-    [6,7,8],
-    [2,4,6]
-]
-let game_ended = false;
-let player1 = 'X';
-let player2 = '0';
+const btnResetScore = document.querySelector('#reset');
+const btnResetGame = document.querySelector('#game');
+const squares = document.querySelectorAll('.col');
+let player1Score = document.querySelector('#player_one_score')
+let player2Score = document.querySelector('#player_two_score')
+const winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+const startGame = document.querySelector('#start')
+const xNameInput = document.querySelector('#x_input');
+const oNameInput = document.querySelector('#o_input');
+const modal = document.querySelector('.modal')
 
-let player1score = 0;
-let player2score = 0;
-
-let currentTurn =  1;
-let movesMade = 0;
-
-const player1NameAsk = prompt("First player's name: ").split('')
-player1Name = player1NameAsk[0].toUpperCase() + player1NameAsk.slice(1).join('')
-const player2NameAsk = prompt("Second player's name: ").split('');
-player2Name = player2NameAsk[0].toUpperCase() + player2NameAsk.slice(1).join('')
-
-function setPlayersNames(){
-    
-
-    document.querySelector('#player_one').innerText = player1Name;
-    document.querySelector('#player_two').innerText = player2Name;
-    document.querySelector('#player_one_score').innerText = player1score;
-    document.querySelector('#player_two_score').innerText = player2score;
-}
-
-document.addEventListener('DOMContentLoaded', setPlayersNames)
-document.querySelector('#reset').addEventListener('click', resetScore)
-document.querySelector('#game').addEventListener('click', resetGame)
+let xColorSquare = []
+let oColorSquare = []
+let makeMoves = 0;
+let xChoices = []
+let oChoices = []
+player1Score = 0
+player2Score = 0
+let currentPlayer = 0;
+game_ended = false
 
 
 
 
-positions.forEach((position) =>{
-    position.addEventListener('click', (e) =>{
-        
-        if(game_ended){
-            if(confirm('Game ended. Do you want to start another game?')){
-                resetGame()
-            }
-            return 
-        }
 
-        movesMade++;
-        
-        if(currentTurn === 1){
-            if(e.currentTarget.innerHTML === ''){
-                e.currentTarget.style.color = 'red'
-                e.currentTarget.innerHTML = player1;
-                currentTurn += 1;
-                e.currentTarget.style.background = 'rgba(172,172,172,0.5)';
-                
-                
-            }      
-        }else{
-            if(e.currentTarget.innerHTML === ''){
-                e.currentTarget.style.color = 'blue'
-                e.currentTarget.innerHTML = player2;
-                currentTurn -= 1;
-                e.currentTarget.style.background = 'rgba(76,76,76,0.8)';
-                
-                     
-            }           
-        }
-                
-    if(checkForWinner()){
-        if(currentTurn === 2){ // X player wins
-            alert(`${player1Name} won this round.`)
-            player1score++
-            document.querySelector('#player_one_score').innerText = player1score;
-            game_ended = true
-            
-        }
-        else{
-            alert(`${player2Name} won this round.`)
-            player2score++
-            document.querySelector('#player_two_score').innerText = player2score
-            game_ended = true
-            
-        }
+
+startGame.addEventListener('click', function(){
+    if(xNameInput.value == '' || oNameInput.value == ''){
+        alert('You need to insert a name !')
     }
 
-    })
-
-    function checkForWinner(){
-        if(movesMade > 4){
-            let moves = Array.prototype.slice.call(positions);
-            let results = moves.map((e)=>{
-                return e.innerHTML})
-        
-        
-            return winningCombo.find((combo) =>{
-                if (results[combo[0]] !== "" && results[combo[1]] !== "" && results[combo[2]] !== "" && results[combo[0]] === results[combo[1]] && results[combo[1]] === results[combo[2]]) {
-                    return true;
-                } else {
-                    return false;
-                }
-            })
-        }      
+    else{
+        player_one.innerText = xNameInput.value;
+        player_two.innerText = oNameInput.value;
+        modal.classList.add('active')
     }
+
 })
 
 
+const Gameboard = (function(){
 
-function resetGame() {
-    game_ended = false;
-    currentTurn = 1;
-    positions.forEach((e) =>{
-        e.innerHTML = ''
-        e.style.background = 'white'
-    })
-}
+    const clearBoard = () =>{
+        squares.forEach(square =>{
+            square.innerText = ''
+        })
+    }
+
+    const restartGame = () =>{
+        game_ended = false;
+        currentPlayer = 0;
+        squares.forEach((e) =>{
+            e.innerText = ''
+        })
+        xChoices = []
+        oChoices = []
+    }
+
+    const resetScore = () =>{
+        player1score = 0;
+        player2score = 0;
+        document.querySelector('#player_one_score').innerText = player1score;
+        document.querySelector('#player_two_score').innerText = player2score;
+    }
+
     
 
 
-function resetScore(){
-    player1score = 0;
-    player2score = 0;
-    document.querySelector('#player_one_score').innerText = player1score;
-    document.querySelector('#player_two_score').innerText = player2score;
-}
+    return {clearBoard,restartGame,resetScore}
 
+
+})()
+
+
+const Winner = (function(){
+
+    const checkForXWinner = () =>{
+        return winningCombos.some(combo =>{
+            return combo.every(e =>{
+                return xChoices.includes(e)
+            })
+        })
+    }
+    
+    const checkForOWinner = () =>{
+        return winningCombos.some(combo =>{
+            return combo.every(e =>{
+                return oChoices.includes(e)
+            })
+        })
+    }
+
+    const getXColor = () =>{
+    const winArrayIndex = winningCombos
+            .map(arr => arr.every(y => xChoices.includes(y)))
+            .findIndex(x => x)
+    for (let i = 0; i < winningCombos[winArrayIndex].length; i++) {
+            xColorSquare.push(winningCombos[winArrayIndex][i])
+          }
+        return xColorSquare
+    }
+
+    const getOColor = () =>{
+        const winArrayIndex = winningCombos
+                .map(arr => arr.every(y => oChoices.includes(y)))
+                .findIndex(x => x)
+        for (let i = 0; i < winningCombos[winArrayIndex].length; i++) {
+                oColorSquare.push(winningCombos[winArrayIndex][i])
+              }
+            return oColorSquare
+        }
+
+
+     
+
+    return {checkForXWinner, checkForOWinner,getXColor, getOColor}
+
+})()
+
+
+squares.forEach(square =>{
+    square.addEventListener('click', function(e){
+        
+    if(game_ended){
+        if(confirm('Game ended. Do you want to start another game?')){
+            Gameboard.restartGame()
+        }
+        return 
+    }
+    
+    
+    
+    makeMoves++
+
+    if(currentPlayer == 0 && square.innerText === ''){
+            square.innerText = 'X'
+            xChoices.push(Number(square.id.replace(/c/g, ''))) 
+            currentPlayer++
+        
+        }
+    else if(currentPlayer == 1 && square.innerText === ''){
+            square.innerText = 'O';
+            oChoices.push(Number(square.id.replace(/c/g, '')))
+            currentPlayer--
+
+    }
+    
+    if(Winner.checkForOWinner()){
+        game_ended = true;
+        player2Score++
+        document.querySelector('#player_two_score').innerText = player2Score;
+        Winner.getOColor();
+        console.log(oColorSquare);
+        
+        
+    }
+    if(Winner.checkForXWinner()){
+        game_ended = true;
+        player1Score++     
+        document.querySelector('#player_one_score').innerText = player1Score;
+        Winner.getXColor();
+        }
     
 
+    if(makeMoves === 9 && Winner.checkForOWinner() === false && Winner.checkForXWinner() === false){
+        setTimeout(function(){
+            alert(`It's a draw !`)
+        }, 600)
+    }
+    
+    
+    }) 
+}) 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+btnResetGame.addEventListener('click', Gameboard.restartGame)
+btnResetScore.addEventListener('click', Gameboard.resetScore)
